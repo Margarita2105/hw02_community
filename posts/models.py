a9154fc46913ebf64952d1fe78397780c405cf74
Margarita <khokhlova2105@yandex.ru>
2020-03-25 17:30:04 +0300

    com2

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -11,6 +12,7 @@ class Group(models.Model):
 
     def __str__(self):
         return self.slug
+
 
 class Post(models.Model):
     text = models.TextField()
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
-
 from .models import Group, Post
+
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
@@ -10,8 +9,6 @@ def index(request):
 
 
 def group_posts(request, slug):
-
     group = get_object_or_404(Group, slug=slug)
-
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
